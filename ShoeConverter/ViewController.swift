205777dadea092805d9cf6f52dd8e1bedcd351a7
Jarl Erik Cedergren <jarlerik.c@gmail.com>
2014-10-12 23:08:50 +0200

    Initial Commit

---
+++ ShoeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeConverter
+//
+//  Created by Jarl Erik Cedergren on 12.10.14.
+//  Copyright (c) 2014 Cedergren Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
